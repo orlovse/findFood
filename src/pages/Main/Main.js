e12d4c1597acd82c29822ec915c5d61fe5a263c3
orlovse <hajoff@gmail.com>
2020-10-09 11:46:49 +0300

    add skeletons

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,37 +2,53 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import { Grid } from "@material-ui/core";
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import { loadRestaurants } from "../../redux/restaurants/action";
 import { restaurantsSelector, restaurantsLoadingSelector, restaurantsLoadedSelector } from "../../redux/restaurants/selectors";
 import { NewsCard, RestaurantCard } from "../../components";
 
+const Main = ({
+        loadRestaurants, 
+        restaurants, 
+        restaurantsLoading, 
+        restaurantsLoaded
+    }) => {
 
-
-
-const Main = ({loadRestaurants, restaurants, loading, loaded}) => {
     useEffect(() => {
-       if(!loading && !loaded) loadRestaurants();
-    }, []);
-    
-    if(loading || !loaded) return <div>Loading...</div>
-    const r = restaurants
-        ? restaurants.map(restaurant => 
-            <Grid item xs={12} sm={6} md={4} key={restaurant.id}>
-                <RestaurantCard restaurant={restaurant}>
-                    {restaurant.name}
-                </RestaurantCard>
-            </Grid>
-        )
-        : <div>loading...</div>
+       if(!restaurantsLoading && !restaurantsLoaded) loadRestaurants(); // eslint-disable-next-line
+    }, []); 
 
+    // restaurantsLoading = true;
+    const restaurantsElement = !restaurantsLoading || restaurantsLoaded
+        ? (
+            <Grid container spacing={3}>
+                {restaurants.map(restaurant => 
+                    <Grid item xs={12} sm={6} md={4} key={restaurant.id}>
+                        <RestaurantCard restaurant={restaurant}>
+                            {restaurant.name}
+                        </RestaurantCard>
+                    </Grid>
+                )}
+            </Grid>
+        ) : (
+            <Grid container spacing={3}>
+                {[1,2,3,4,5,6].map(skeletonItem => (
+                    <Grid item xs={12} sm={6} md={4} key={skeletonItem}>
+                        <Skeleton variant="rect" height={140} />
+                        <Skeleton />
+                        <Skeleton width="60%" />
+                    </Grid>
+                ))}
+            </Grid>        
+        );
 
     return (
         <div>
             Main
             <NewsCard />
             <Grid container spacing={3}>
-                {r}
+                {restaurantsElement}
             </Grid>
 
         </div>
@@ -41,14 +57,8 @@ const Main = ({loadRestaurants, restaurants, loading, loaded}) => {
 
 const mapStateToProps = (state) => ({
     restaurants: restaurantsSelector(state),
-    loading: restaurantsLoadingSelector(state),
-    loaded: restaurantsLoadedSelector(state),
+    restaurantsLoading: restaurantsLoadingSelector(state),
+    restaurantsLoaded: restaurantsLoadedSelector(state),
 });
-
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         actions: bindActionCreators({ loadRestaurants }, dispatch)
-//     }
-// };
 
 export default connect(mapStateToProps ,{loadRestaurants})(Main);
--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
-import { Grid } from "@material-ui/core";
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import { loadRestaurant } from "../../redux/restaurant/action"
 import { loadMenu } from "../../redux/menu/action";
@@ -10,22 +11,44 @@ import { restaurantSelector, restaurantLoadingSelector, restaurantLoadedSelector
 
 import { BasketProducts ,MenuItem } from "../../components";
 
-const Restaurant = ({restaurant, menu, loadRestaurant, loadMenu, restaurantLoading, menuLoading, restaurantLoaded, menuLoaded}) => {
+const Restaurant = ({
+        loadRestaurant, 
+        loadMenu, 
+        restaurantLoading, 
+        menuLoading, 
+        restaurantLoaded, 
+        menuLoaded,
+        restaurant, 
+        menu
+    }) => {
+
     const { id } = useParams();
 
     useEffect(() => {
-        if(!restaurantLoading && !restaurantLoaded) loadRestaurant(id);
+        if(!restaurantLoading && !restaurantLoaded) loadRestaurant(id); // eslint-disable-next-line
     }, []);
 
     useEffect(() => {
-        if(!menuLoading && !menuLoaded) loadMenu(id)
+        if(!menuLoading && !menuLoaded) loadMenu(id) // eslint-disable-next-line
     }, []);
 
+    // restaurantLoading = true;
     const restaurantElem = restaurantLoading || !restaurantLoaded
-        ? <div>Restaurant loading...</div>
-        : (
+        ? (
             <>
-            <h2>Address</h2>
+                <Typography component="div" variant={"h2"}>
+                    <Skeleton />
+                </Typography>
+                <Skeleton />
+                <Typography component="div" variant={"h2"}>
+                    <Skeleton />
+                </Typography>
+                <Skeleton />
+                <Skeleton />
+            </>
+        ): (
+            <>
+                <h2>Address</h2>
                 <p>{restaurant.address}</p>
                 <h2>Time working</h2>
                 <p>12-24</p>
@@ -33,8 +56,27 @@ const Restaurant = ({restaurant, menu, loadRestaurant, loadMenu, restaurantLoadi
             </>
         );
 
+    // menuLoading = true;
     const menuElem = menuLoading || !menuLoaded
-        ? <div>Menu loading...</div>
+        ? [1,2,3,4].map(skeletonItem => (
+                    <Card key={skeletonItem} style={{margin: "20px"}}>
+                        <Grid container>
+                            <Grid item xs={8}>
+                                <CardContent>
+                                    <Typography component="h5" variant="h5">
+                                        <Skeleton />
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                        <Skeleton />
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Skeleton variant="rect" height={"100%"} />
+                            </Grid>
+                        </Grid>
+                    </Card>                  
+                ))
         : menu.map((menuItem, i) => <MenuItem menuItem={menuItem} restaurantId={restaurant.id} key={i} /> )
 
     return (
